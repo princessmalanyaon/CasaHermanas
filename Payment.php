b7151ed935ab216ef4f54b198be95fe23d91a4fc
fiveten09092902988 <130917118+fiveten09092902988@users.noreply.github.com>
2024-05-17 01:20:37 +0800

    MyUpdate

---
+++ Payment.php
@@ -0,0 +1,50 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+
+$UserFullName = $_POST["UserFullName"];
+$UserEmail = $_POST["UserEmail"];
+$UserPassword = $_POST["UserPassword"];
+
+
+try{
+    //Links to Connection.php
+    require_once "Connection1.php";
+
+    
+
+    $query = "INSERT INTO user_account(UserFullName, UserEmail, UserPassword) values
+    (:UserFullName, :UserEmail, :UserPassword);";
+
+    $stmt = $conn->prepare($query);
+
+    $option = [
+        'cost' => 12
+    ];
+
+
+
+    $hashPassword = password_hash($UserPassword, PASSWORD_BCRYPT, $option);
+    $stmt->bindParam(":UserFullName", $UserFullName);
+    $stmt->bindParam(":UserEmail", $UserEmail);
+    $stmt->bindParam(":UserPassword", $UserPassword);
+
+
+    $stmt->execute();
+
+    $conn = null;
+    $stmt = null;
+    header("Location: http://127.0.0.1/CasaHermanasPrincess/index.html");
+    die();
+    */
+}
+//Error handling naman to incase na di gumana yung sa try catch block
+catch(PDOException $e){
+    die("Query Failed: " . $e->getMessage());
+}
+
+}else{
+    header("Location: .../login-signup.php");
+}
